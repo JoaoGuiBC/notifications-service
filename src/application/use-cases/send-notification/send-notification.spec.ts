5160cc4eeeedf053da0d980ad33e46260275549b
JoaoGuiBC <joaoguibc@gmail.com>
2022-12-15 17:25:40 +0000

    Create in memory database

--- src/application/use-cases/send-notification/send-notification.spec.ts
+++ src/application/use-cases/send-notification/send-notification.spec.ts
@@ -1,24 +1,18 @@
+import { inMemoryNotificationsRepository } from "../../../../test/repositories/in-memory-notifications-repository";
 import { SendNotification } from ".";
-import { Notification } from '../../entities/notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  }
-}
 
 describe('Send notification', () => {
   it('Should be able to send a notification', async () => {
+    const notificationsRepository = new inMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: 'Esta é uma notificação',
       category: 'Work',
       recipientId: 'example-recipient-id'
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toEqual(notification);
   });
 });
